970dae750781c0da6394f0dce97ac21f74c95cef
Denis Gomes <denisgomes938@gmail.com>
2023-01-02 12:27:13 -0300

    feat:pagina home finalizada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -41,14 +41,91 @@ export function Home() {
         </S.ProfileCard>
       </header>
 
-      <form>
+      <S.SearchForm>
         <div>
-          <span></span>
-          <span></span>
-
-          <input type="text" />
+          <span>Publicações</span>
+          <span>6 publicações</span>
         </div>
-      </form>
+
+        <input type="text" placeholder="Buscar conteúdo" />
+      </S.SearchForm>
+
+      <S.ListOfPosts>
+        <S.PostContainer>
+          <S.PostContent>
+            <div>
+              <S.PostTitle>
+                JavaScript data types and data structures
+              </S.PostTitle>
+              <S.PostTime>Há 1 dia</S.PostTime>
+            </div>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic language. Variables in JavaScript are not directly
+              associated with any particular value type, and any variable can be
+              assigned (and re-assigned) values of all types: let foo = 42; //
+              foo is now a number foo = &apos;bar&rsquo;; // foo is now a string
+              foo = true; // foo is now a boolean
+            </p>
+          </S.PostContent>
+        </S.PostContainer>
+
+        <S.PostContainer>
+          <S.PostContent>
+            <div>
+              <S.PostTitle>
+                JavaScript data types and data structures
+              </S.PostTitle>
+              <S.PostTime>Há 1 dia</S.PostTime>
+            </div>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic language. Variables in JavaScript are not directly
+              associated with any particular value type, and any variable can be
+              assigned (and re-assigned) values of all types: let foo = 42; //
+              foo is now a number foo = &apos;bar&rsquo;; // foo is now a string
+              foo = true; // foo is now a boolean
+            </p>
+          </S.PostContent>
+        </S.PostContainer>
+
+        <S.PostContainer>
+          <S.PostContent>
+            <div>
+              <S.PostTitle>
+                JavaScript data types and data structures
+              </S.PostTitle>
+              <S.PostTime>Há 1 dia</S.PostTime>
+            </div>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic language. Variables in JavaScript are not directly
+              associated with any particular value type, and any variable can be
+              assigned (and re-assigned) values of all types: let foo = 42; //
+              foo is now a number foo = &apos;bar&rsquo;; // foo is now a string
+              foo = true; // foo is now a boolean
+            </p>
+          </S.PostContent>
+        </S.PostContainer>
+      </S.ListOfPosts>
     </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,9 +4,7 @@ export const Container = styled.main`
   margin: 0 19rem;
   margin-top: -5rem;
 
-  /* z-index: 0; */
   display: block;
-  position: fixed;
 `
 export const ProfileCard = styled.div`
   background-color: ${(props) => props.theme.baseProfile};
@@ -15,6 +13,8 @@ export const ProfileCard = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  position: relative;
 `
 
 export const AvatarCard = styled.div`
@@ -87,4 +87,109 @@ export const CardSocial = styled.div`
     justify-content: center;
     align-items: center;
   }
+`
+
+export const SearchForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  margin-top: 4.5rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    span:first-child {
+      color: ${(props) => props.theme.baseSubtitle};
+      font-weight: 700;
+      font-size: 1.2rem;
+      line-height: 160%;
+    }
+
+    span:last-child {
+      font-weight: 400;
+      font-size: 0.875rem;
+      line-height: 160%;
+      color: ${(props) => props.theme.baseSpan};
+    }
+  }
+
+  input {
+    background: ${(props) => props.theme.baseInput};
+
+    border: 1px solid ${(props) => props.theme.baseBorder};
+    border-radius: 6px;
+
+    color: ${(props) => props.theme.baseLabel};
+    padding: 12px 16px;
+
+    ::placeholder {
+      color: ${(props) => props.theme.baseLabel};
+    }
+  }
+`
+
+export const ListOfPosts = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 2rem;
+
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
+`
+
+export const PostContainer = styled.div`
+  width: calc((100vw - 40rem) / 2);
+  height: 16.25rem;
+
+  background: ${(props) => props.theme.basePost};
+  border-radius: 10px;
+`
+
+export const PostContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: start;
+  padding: 2rem;
+  gap: 1.25rem;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    color: ${(props) => props.theme.baseText};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-height: 100px;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 4; // Quantidade de linhas
+    -webkit-box-orient: vertical;
+  }
+`
+
+export const PostTitle = styled.span`
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 160%;
+  color: ${(props) => props.theme.baseTitle};
+`
+
+export const PostTime = styled.span`
+  font-weight: 400;
+  font-size: 0.875rem;
+  white-space: nowrap;
+  color: ${(props) => props.theme.baseSpan};
 `
